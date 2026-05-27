980362aa492efd90fc7330443bca4a90e27aeaa3
k1do3 <k1buwork@gmail.com>
2026-05-27 21:20:51 +0300

    add unit2 from other rep

---
+++ Assets/LessonCode2/Program.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseStart
+{
+    //Необходим раскомментировать нужные блоки в процессе просмотра видео
+    //Для того чтобы раскомментировать код, нужно удалить /* */
+    //Код, который находится под комментированием не используется для выполнения
+    internal class Program
+    {
+
+        class House
+        {
+            public string street;
+            public int number;
+
+            public void Print()
+            {
+                Console.WriteLine($"Улица {street}, {number}");
+            }
+        }
+
+
+        static void TestFunc(ref int n)
+        {
+            n = 10;
+            Console.WriteLine(n);
+        }
+
+        static void Main(string[] args)
+        {
+            //int a = 5;
+            //int b = 5;
+            //int c = 10;
+            //if (b > a)
+            //{
+            //    Console.WriteLine($"Число {b} больше числа {a}");
+            //}
+            //else if (b < a)
+            //{
+            //    Console.WriteLine($"Число {b} меньше числа {a}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Число {b} равно числу {a}");
+            //}
+
+            //for (int i = 1; i < 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+
+            //int count = 0;
+            //while (count <= 10)
+            //{
+            //    count++;
+            //    Console.WriteLine(count);
+            //}
+
+
+
+            //int[] numbers = new int[5];
+            //int[] numbers2 = { 5, 6, 7, 8, 9 };
+            //numbers2[4] = 15;
+            //Console.WriteLine(numbers2[4]);
+
+
+
+            //void Helloworld()
+            //{
+            //    Console.WriteLine("Hello World");
+            //}
+
+            //void Sum(int first, int sec)
+            //{
+            //    int sum = first + sec;
+            //    Console.WriteLine(sum);
+            //}
+
+            //void Human(string name, int age)
+            //{
+            //    Console.WriteLine($"{name}, {age}");
+            //}
+            //Human("Кирилл", 22);
+
+
+
+            //House tower = new House();
+            //tower.street = "Красная";
+            //tower.number = 50;
+
+            //tower.Print();
+
+
+            // 2.3
+            short s = 0;
+            int i = 0;
+            long l = 0;
+            float f = 0;
+            double d = 0;
+
+
+            object o = d;
+            double d2 = (double)o;
+
+            ushort a = 15050;
+            byte b = (byte)a;
+
+
+
+            i = (int)l;
+            l = i;
+
+
+            TestFunc(ref i);
+
+
+            var c = 0f;
+
+
+            if (o != null)
+            {
+                int i2 = Convert.ToInt32(o);
+            }
+
+            int[] array = new int[10000];
+
+
+            List<int> list = new List<int>(80);
+        }
+    }
+}
